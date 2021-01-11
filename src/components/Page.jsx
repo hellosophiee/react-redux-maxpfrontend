595e5eaad06a720547fcd6748386497adabe466e
Sophie <50250729+hellosophiee@users.noreply.github.com>
2021-01-11 18:09:49 +0500

    feat: making actions (update Redux app - dispatch action && react on action in the reducer)

--- src/components/Page.jsx
+++ src/components/Page.jsx
@@ -2,10 +2,22 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 export class Page extends Component {
+	onBtnClick = (e) => {
+		const year = +e.currentTarget.innerText;
+		this.props.setYear(year);
+	};
+
 	render() {
 		const { year, photos } = this.props;
 		return (
 			<div>
+				<div>
+					<button onClick={this.onBtnClick}>2018</button>
+					<button onClick={this.onBtnClick}>2017</button>
+					<button onClick={this.onBtnClick}>2016</button>
+					<button onClick={this.onBtnClick}>2015</button>
+					<button onClick={this.onBtnClick}>2014</button>
+				</div>
 				<p>
 					У тебя {photos.length} фото за {year} год
 				</p>
@@ -16,5 +28,6 @@ export class Page extends Component {
 
 Page.propTypes = {
 	year: PropTypes.number.isRequired,
-	photos: PropTypes.array.isRequired
+	photos: PropTypes.array.isRequired,
+	setYear: PropTypes.func.isRequired
 };
--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -2,19 +2,20 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { User } from '../components/User';
 import { Page } from '../components/Page';
+import { setYear } from '../actions/PageActions';
 
 import './styles.css';
 
 class App extends Component {
 	render() {
-		const { user, page } = this.props;
+		const { user, page, setYearAction } = this.props;
 		return (
 			<div className="App">
 				<header className="App-header">
 					<h1 className="App-title">Мой топ фото</h1>
 				</header>
 				<User name={user.name} />
-				<Page photos={page.photos} year={page.year} />
+				<Page photos={page.photos} year={page.year} setYear={setYearAction} />
 			</div>
 		);
 	}
@@ -28,4 +29,8 @@ const mapStateToProps = (store) => {
 	};
 };
 
-export default connect(mapStateToProps)(App);
+const mapDispatchToProps = (dispatch) => ({
+	setYearAction: (year) => dispatch(setYear(year))
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
