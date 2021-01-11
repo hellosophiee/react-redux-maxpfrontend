2ed4d019ec7d966d0083bb6257a43f18226f5ad8
Sophie <50250729+hellosophiee@users.noreply.github.com>
2021-01-11 16:09:27 +0500

    feat: containers && components

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { User } from '../components/User';
+import { Page } from '../components/Page';
+
 import './styles.css';
 
 class App extends Component {
@@ -10,10 +13,8 @@ class App extends Component {
 				<header className="App-header">
 					<h1 className="App-title">Мой топ фото</h1>
 				</header>
-				<p>Привет, {user.name}!</p>
-				<p>
-					У тебя {page.photos.length} фото за {page.year} год
-				</p>
+				<User name={user.name} />
+				<Page photos={page.photos} year={page.year} />
 			</div>
 		);
 	}
--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { Provider } from "react-redux";
-import { store } from "./store/configureStore";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { store } from './store/configureStore';
 
-import App from "./App";
+import App from './containers/App';
 
-const rootElement = document.getElementById("root");
+const rootElement = document.getElementById('root');
 ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  rootElement
+	<Provider store={store}>
+		<App />
+	</Provider>,
+	rootElement
 );
