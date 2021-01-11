5bfe9b3e4441fd36ab9eb73939828bc4a18e583c
Sophie <50250729+hellosophiee@users.noreply.github.com>
2021-01-11 15:34:02 +0500

    feat: combine reducers

--- src/reducers/index.jsx
+++ src/reducers/index.jsx
@@ -1,11 +1,8 @@
-export const initialState = {
-	user: {
-		name: 'Василий',
-		lastname: 'Реактов',
-		age: 18
-	}
-};
+import { combineReducers } from 'redux';
+import { pageReducer } from './page';
+import { userReducer } from './user';
 
-export function rootReducer(state = initialState) {
-	return state;
-}
+export const rootReducer = combineReducers({
+	page: pageReducer,
+	user: userReducer
+});
