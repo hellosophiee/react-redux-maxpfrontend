d28dce20d0e8d7305ea4de13322b527bf0f241a2
Sophie <50250729+hellosophiee@users.noreply.github.com>
2021-01-11 13:52:20 +0500

    feat: configure the entry point for the redux app, prettier && css styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
-import React from "react";
-import "./styles.css";
+import React, { Component } from 'react';
+import './styles.css';
 
 export default function App() {
-  return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
-  );
+	return (
+		<div className="App">
+			<header className="App-header">
+				<h1 className="App-title">Мой топ фото</h1>
+			</header>
+			<p className="App-intro">Здесь будут мои самые залайканые фото</p>
+		</div>
+	);
 }
